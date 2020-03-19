5b013d189767c91c9901c6dd3236c710bc89f841
Denis Tarasov <dstarasov@skbkontur.ru>
2020-03-19 12:41:21 +0500

    move Commons.WellKnownLogScopes to Helpers.MicrosoftLogScopes move ActionLogScopeOld and HostingLogScopeOld from vostok.applications.aspnetcore library

--- Vostok.Logging.Microsoft/Helpers/MicrosoftLogScopes.cs
+++ Vostok.Logging.Microsoft/Helpers/MicrosoftLogScopes.cs
@@ -1,14 +1,20 @@
 ﻿using JetBrains.Annotations;
 
-namespace Vostok.Logging.Microsoft.Commons
+namespace Vostok.Logging.Microsoft.Helpers
 {
     [PublicAPI]
-    public static class WellKnownLogScopes
+    public static class MicrosoftLogScopes
     {
         public const string ActionLogScope = "Microsoft.AspNetCore.Mvc.MvcCoreLoggerExtensions+ActionLogScope";
+        public const string ActionLogScopeOld = "Microsoft.AspNetCore.Mvc.Internal.MvcCoreLoggerExtensions+ActionLogScope";
+
         public const string HostingLogScope = "Microsoft.AspNetCore.Hosting.HostingLoggerExtensions+HostingLogScope";
+
         public const string ConnectionLogScope = "Microsoft.AspNetCore.Server.Kestrel.Core.Internal.ConnectionLogScope";
+        public const string HostingLogScopeOld = "Microsoft.AspNetCore.Hosting.Internal.HostingLoggerExtensions+HostingLogScope";
+
         public const string HealthCheckLogScope = "Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckLogScope";
+
         public const string ViewComponentLogScope = "Microsoft.AspNetCore.Mvc.ViewFeatures.MvcViewFeaturesLoggerExtensions+ViewComponentLogScope";
     }
 }
