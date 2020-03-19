f3d2b05b7869a92e0ca999e1b7df1a9929cf1f86
Denis Tarasov <dstarasov@skbkontur.ru>
2020-03-19 12:04:28 +0500

    add well-known ASPNET.Core log scopes

---
+++ Vostok.Logging.Microsoft/Commons/WellKnownLogScopes.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Logging.Microsoft.Commons
+{
+    [PublicAPI]
+    public static class WellKnownLogScopes
+    {
+        public const string ActionLogScope = "Microsoft.AspNetCore.Mvc.MvcCoreLoggerExtensions+ActionLogScope";
+        public const string HostingLogScope = "Microsoft.AspNetCore.Hosting.HostingLoggerExtensions+HostingLogScope";
+        public const string ConnectionLogScope = "Microsoft.AspNetCore.Server.Kestrel.Core.Internal.ConnectionLogScope";
+        public const string HealthCheckLogScope = "Microsoft.Extensions.Diagnostics.HealthChecks.HealthCheckLogScope";
+        public const string ViewComponentLogScope = "Microsoft.AspNetCore.Mvc.ViewFeatures.MvcViewFeaturesLoggerExtensions+ViewComponentLogScope";
+    }
+}
